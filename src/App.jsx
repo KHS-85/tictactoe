dabf0765e11bf61bba3e570d9267805901bc0df2
KHS-85 <60042770+KHS-85@users.noreply.github.com>
2020-03-16 17:19:41 +0100

    A game of Tic Tac Toe

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Game from './components/game';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tic Tac Toe</h1>
+      <Game></Game>
+    </div>
+  );
+}
+
+export default App;
